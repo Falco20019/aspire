b2b1d0f3f784c5602748d0266c8e5be82f7cd61c
James Newton-King <james@newtonking.com>
2026-01-30 12:18:12 +0800

    Fix warnings

--- tests/Aspire.Cli.Tests/Packaging/TemporaryNuGetConfigTests.cs
+++ tests/Aspire.Cli.Tests/Packaging/TemporaryNuGetConfigTests.cs
@@ -1,7 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using Microsoft.AspNetCore.InternalTesting;
 using System.Xml;
 using Aspire.Cli.Packaging;
 
